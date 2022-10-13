b04682916ee93e61028e1210b3fc404736dccb23
Graham Bygrave <graham@bygrave.org>
2022-10-13 09:00:48 +0100

    Initial commit of new dummy failover service (MVC not Reactive).

---
+++ src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
@@ -0,0 +1,35 @@
+package guru.sfg.beer.inventory.service.web.controllers;
+
+import guru.sfg.brewery.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class BeerInventoryController {
+
+    @GetMapping("api/v1/beer/{beerId}/inventory-failover")
+    List<BeerInventoryDto> listBeersById(@PathVariable UUID beerId){
+        OffsetDateTime now = OffsetDateTime.now();
+        BeerInventoryDto beerInventoryDto = BeerInventoryDto.builder()
+                .id(new UUID(0,0))
+                .beerId(new UUID(0,0))
+                .quantityOnHand(999)
+                .createdDate(now)
+                .lastModifiedDate(now)
+                .build();
+        return Collections.singletonList(beerInventoryDto);
+    }
+}
